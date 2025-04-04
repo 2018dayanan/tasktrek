cdc019d82108cd2cf222d84d4ea62d406335e6bc
2018dayanan <daychy72@gmail.com>
2025-04-04 17:12:09 +0545

    feat: complete TaskList component implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import "./App.css";
 import TaskForm from './components/TaskForm';
+import TaskColumn from './components/TaskColumn';
 const App = () => {
   return (
     <div className='app'>
       <TaskForm></TaskForm>
       <main className="app_main">
-        <section className="task_column">Section 1</section>
-        <section className="task_column">Section 2</section>
-        <section className="task_column">Section 3</section>
+        <TaskColumn></TaskColumn>
+        <TaskColumn></TaskColumn>
+        <TaskColumn></TaskColumn>
+       
       </main>
     </div>
   )
---
+++ src/components/TaskColumn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Todo from '../assets/direct-hit.png';
+import './TaskColumn.css';
+const TaskColumn = () => {
+    return (
+        <section className="task_column">
+            <h2 className='task_column_heading'><img className='task_column_icon' src={Todo} /> To do</h2>
+        </section>
+
+    );
+};
+
+export default TaskColumn;
