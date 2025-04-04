dc5609c9680adaed18c6e12443a64e256ce445f3
2018dayanan <daychy72@gmail.com>
2025-04-04 18:19:15 +0545

    feat: add props support in TaskColumn component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import "./App.css";
 import TaskForm from './components/TaskForm';
 import TaskColumn from './components/TaskColumn';
+import todoicon from './assets/direct-hit.png'
+import doingIcon from './assets/glowing-star.png'
+import doneIcon from './assets/check-mark-button.png'
 const App = () => {
   return (
     <div className='app'>
       <TaskForm></TaskForm>
       <main className="app_main">
-        <TaskColumn></TaskColumn>
-        <TaskColumn></TaskColumn>
-        <TaskColumn></TaskColumn>
-       
+        <TaskColumn title="To do" icon={todoicon}></TaskColumn>
+        <TaskColumn title="Doing" icon={doingIcon}></TaskColumn>
+        <TaskColumn title="Done" icon={doneIcon}></TaskColumn>
       </main>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Todo from '../assets/direct-hit.png';
 import './TaskColumn.css';
-const TaskColumn = () => {
+const TaskColumn = ({title, icon}) => {
     return (
         <section className="task_column">
-            <h2 className='task_column_heading'><img className='task_column_icon' src={Todo} /> To do</h2>
+            <h2 className='task_column_heading'><img className='task_column_icon' src={icon} /> {title}</h2>
         </section>
 
     );
