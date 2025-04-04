2afd501679e065e72af14ae1190c8a8ca3d6e026
2018dayanan <daychy72@gmail.com>
2025-04-04 16:19:29 +0545

    Styles added for form component

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./TaskForm.css";
 
 const TaskForm = () => {
     return (
@@ -6,20 +7,23 @@ const TaskForm = () => {
             <form action="">
                 <input type="text" className='task_input' placeholder='Enter Your Task' />
                 <div className='task_form_bottom_line'>
-                    <button className='tag'>HTML</button>
-                    <button className='tag'>CSS</button>
-                    <button className='tag'>JavaScript</button>
-                    <button className='tag'>React</button>
-                    <select>
-                        <option value="todo">To do</option>
-                        <option value="doing">To do</option>
-                        <option value="done">Done</option>
-                    </select>
-                    <button type='submit' className='task_submit'>+ Add Task</button>
+                    <div>
+                        <button className='tag'>HTML</button>
+                        <button className='tag'>CSS</button>
+                        <button className='tag'>JavaScript</button>
+                        <button className='tag'>React</button>
+                    </div>
+                    <div>
+                        <select className='task_status'>
+                            <option value="todo">To do</option>
+                            <option value="doing">To do</option>
+                            <option value="done">Done</option>
+                        </select>
+                        <button type='submit' className='task_submit'>+ Add Task</button>
+                    </div>
                 </div>
             </form>
         </header>
-
     )
 }
 
