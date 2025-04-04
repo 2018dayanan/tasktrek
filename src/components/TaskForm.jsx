3f12a9730d8ce6e355aeef937082b83dc04d10d9
2018dayanan <daychy72@gmail.com>
2025-04-04 19:13:22 +0545

    feat: implement form handling functionality

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./TaskForm.css";
 import Tag from './Tag';
 
 const TaskForm = () => {
+    const [task, setTask] = useState("");
+    const [status, setStatus] = useState("todo");
+    const handleStatusChange = (e) => {
+        setStatus(e.target.value)
+    }
+    const handleTaskChange = (e) => {
+        setTask(e.target.value);
+    }
+    console.log(task, status)
+
     return (
         <header className="app_header">
             <form action="">
-                <input type="text" className='task_input' placeholder='Enter Your Task' />
+                <input type="text" className='task_input' placeholder='Enter Your Task' onChange={handleTaskChange} />
                 <div className='task_form_bottom_line'>
                     <div>
                         <Tag tagname="HTML"></Tag>
@@ -15,7 +25,7 @@ const TaskForm = () => {
                         <Tag tagname="React"></Tag>
                     </div>
                     <div>
-                        <select className='task_status'>
+                        <select className='task_status' onChange={handleStatusChange}>
                             <option value="todo">To do</option>
                             <option value="doing">doing</option>
                             <option value="done">Done</option>
