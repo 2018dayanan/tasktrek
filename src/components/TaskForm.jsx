39fce995951ff4b4bdae57c80881b6e314263078
2018dayanan <daychy72@gmail.com>
2025-04-05 12:20:00 +0545

    chore(form): add shortcut trick to handle complex form

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -3,20 +3,39 @@ import "./TaskForm.css";
 import Tag from './Tag';
 
 const TaskForm = () => {
-    const [task, setTask] = useState("");
-    const [status, setStatus] = useState("todo");
-    const handleStatusChange = (e) => {
-        setStatus(e.target.value)
+    // const [task, setTask] = useState("");
+    // const [status, setStatus] = useState("todo");
+    // const handleStatusChange = (e) => {
+    //     setStatus(e.target.value)
+    // }
+    // const handleTaskChange = (e) => {
+    //     setTask(e.target.value);
+    // }
+    // console.log(task, status)
+    const [taskData, setTaskData] = useState({
+        task: "",
+        status: "todo"
+    })
+    const handleChange = (e) => {
+        // const name = e.target.name;
+        // const value = e.target.value;
+        // console.log(name, value)
+        const { name, value } = e.target;
+        setTaskData((prev) => {
+            return { ...prev, [name]: value }
+        })
     }
-    const handleTaskChange = (e) => {
-        setTask(e.target.value);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(taskData)
+
     }
-    console.log(task, status)
+
 
     return (
         <header className="app_header">
-            <form action="">
-                <input type="text" className='task_input' placeholder='Enter Your Task' onChange={handleTaskChange} />
+            <form action="" onSubmit={handleSubmit}>
+                <input type="text" name="task" className='task_input' placeholder='Enter Your Task' onChange={handleChange} />
                 <div className='task_form_bottom_line'>
                     <div>
                         <Tag tagname="HTML"></Tag>
@@ -25,12 +44,12 @@ const TaskForm = () => {
                         <Tag tagname="React"></Tag>
                     </div>
                     <div>
-                        <select className='task_status' onChange={handleStatusChange}>
+                        <select className='task_status' name="status" onChange={handleChange}>
                             <option value="todo">To do</option>
                             <option value="doing">doing</option>
                             <option value="done">Done</option>
                         </select>
-                        <button type='submit' className='task_submit'>+ Add Task</button>
+                        <button type='submit' className='task_submit' >+ Add Task</button>
                     </div>
                 </div>
             </form>
